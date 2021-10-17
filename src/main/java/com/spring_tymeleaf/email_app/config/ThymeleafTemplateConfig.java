7db51115fe1d4e7fb57fd9e8f403612c90b543ac
LalithK90 <asakahatapitiya@gmail.com>
2021-10-17 12:30:50 +0530

    unnecessary config was removed

--- src/main/java/com/spring_tymeleaf/email_app/config/ThymeleafTemplateConfig.java
+++ src/main/java/com/spring_tymeleaf/email_app/config/ThymeleafTemplateConfig.java
@@ -1,40 +1,18 @@
 package com.spring_tymeleaf.email_app.config;
 
 
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
-//@Configuration
-//public class ThymeleafTemplateConfig {
-//
-//  @Bean
-//  public SpringTemplateEngine springTemplateEngine() {
-//    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//    templateEngine.addTemplateResolver(htmlTemplateResolver());
-//    return templateEngine;
-//  }
-//
-//  @Bean
-//  public SpringResourceTemplateResolver htmlTemplateResolver(){
-//    SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-//    emailTemplateResolver.setPrefix("/templates/email");
-//    emailTemplateResolver.setSuffix(".html");
-//    emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
-//    emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//    return emailTemplateResolver;
-//  }
-//}
+import java.util.Collections;
+
 
 @Configuration
 public class ThymeleafTemplateConfig implements WebMvcConfigurer {
